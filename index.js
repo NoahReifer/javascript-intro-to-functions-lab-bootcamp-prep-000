61fe7cbdcc00978f14953910ffd5ca04aa2c59ef
Noah Reifer <sreifer4@gmail.com>
2019-04-04 19:25:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ function shout(string) { return string.toUpperCase() }
 function whisper(strang) { return strang.toLowerCase() }
 function logShout(streng) { console.log(`${streng.toUpperCase()}`) }
 function logWhisper(streng) { console.log(`${streng.toLowerCase()}`) }
-function sayHiToGrandma(string) { if (string === string.toUpperCase()) { return 'YES INDEED!' } else if (string === string.toLowerCase()) { return 'I can\'t hear you!' } }
+function sayHiToGrandma(string) { if (string === string.toUpperCase()) { return 'YES INDEED!' } else if (string === string.toLowerCase()) { return 'I can\'t hear you!' } else if (string === 'I love you, Grandma.') { return 'I love you, too.' } }
